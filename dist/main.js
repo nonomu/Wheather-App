56b169765067f7fad86f001978a1a2cffcfc3bcd
nonomu <noammualmi@gmail.com>
2019-11-04 15:27:52 +0200

    finish delete and post route + request from user(in main)

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,36 @@
 const render = new Renderer()
 
 
-
+const newCity = {
+    name: "Israel",
+    temperature: "23",
+    condition: "Sunny",
+    conditionPic: "lovely"
+}
 
 const fetchWheatherData = function () {
     let input = $("#WheatherCityName").val()
     $.get(`/city/${input}`, function (CityWeather) {
         console.log(CityWeather)
-        
         // render.render(TeamData)
     })
 }
-    // $.post(`team/` , newteams , function (response) {
-    //     //console.log(response)
-    // })
+$.get(`/cities/`, function (cities) {
+    console.log(cities)
+})
+
+$.post(`/city`, newCity, function (cities) {
+    console.log(cities)
+})    
+cityName="Israel"
+
+const deleteWheatherData= function()
+    {
+$.ajax({
+    url: `/city/${cityName}`,
+    type: 'DELETE',
+    success: function(result) {
+        console.log(result)
+    }
+})
+    }
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const moment = require('moment')
 const requestPromise = require("request-promise")
+const WheatherDB = require("../models/City")
 
 const APPID = "693487d5ce7f67db0872c3ce4dbe3b15"
 const WheatherAPIbasicURL = "https://api.openweathermap.org/data/2.5/weather"
 
-
+// const data = new WheatherDB({name:"Noam", temperature :23, condition: "hot", conditionPic:"nana"})
+// data.save()
 
 router.get('/city/:cityName', async function (req, res) {
     const cityName = req.params.cityName
@@ -22,15 +24,19 @@ router.get('/city/:cityName', async function (req, res) {
 
 })
 router.get('/cities', async function (req, res) {
-    const cityName = req.params.cityName
-    try {
-        const weatherData = await await requestPromise(`${WheatherAPIbasicURL}?q=${cityName}&units=metric&APPID=${APPID}`)
-        res.send(JSON.parse(weatherData))
-    }
-    catch (err) {
-        res.send(`city not found`)
-    }
+      const allCities=await WheatherDB.find({})
+        res.send(allCities)
+})
 
+router.post('/city', async function (req, res) {
+    const newCity =new WheatherDB(req.body)
+    newCity.save()
+      res.send("Saved")
+})
+router.delete('/city/:cityName', async function (req, res) {
+    const cityName =req.params.cityName
+    await WheatherDB.deleteOne({name:cityName})
+    res.send("deleted")
 })
 // router.post('/new', function (req, res) {
 
